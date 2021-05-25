88f077925b1157031ef97b58101d739689dae3e7
Stephen Pierzchala <pierzcha@usc.edu>
2021-05-25 14:59:31 -0500

    test

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = (props) => {
 	return (
 		<footer className="Footer">
-			<small>&copy; Copyright 2021, Steve Pierzchala</small>
+			<small>&copy; Copyright 2021, Steve Pierzchala test</small>
 		</footer>
 	);
 };
